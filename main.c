7ad2cce5f3a43e3a29c15ba0c1e72620fe4ad491
ohadwolfman <ohad1696@gmail.com>
2023-01-14 22:22:08 +0200

    #3 commit

--- main.c
+++ main.c
@@ -9,30 +9,49 @@ void multipleShortestPath();
 
 int main() {
     char c;
-    while(scanf("%c",&c)!=EOF);
-    if (c=='A'){
-        createNewGraph(char input);
+    while (scanf("%c", &c) != EOF){
+        if (c == 'A') {
+            while (scanf("%c", &c) != EOF && (c!='A'&&'B'&&'D'&&'S'&&'T')) {
+                createNewGraph();
+            }
+        }
+        if (c == 'B') {
+            while (scanf("%c", &c) != EOF && (c!='A'&&'B'&&'D'&&'S'&&'T')) {
+                createNode(c);
+            }
+        }
+        if (c == 'D') {
+            while (scanf("%c", &c) != EOF);
+            delete_node(
+            char input);
+        }
+        if (c == 'S') {
+            while (scanf("%c", &c) != EOF);
+            shortestPath(
+            char input);
+        }
+        if (c == 'T') {
+            while (scanf("%c", &c) != EOF);
+            multipleShortestPath(
+            char input);
     }
-    if (c=='B'){
-        createNode(char input);
-    }
-    if (c=='D'){
-        delete_node(char input);
-    }
-    if (c=='S'){
-        shortestPath(char input);
-    }
-    if (c=='T'){
-        multipleShortestPath(char input);
-    }
+}
     return 0;
 }
 void createNewGraph() {
-
+    char c;
+    while (scanf("%c", &c) != EOF && (c!='A'&&'B'&&'D'&&'S'&&'T')){
+        if(c=='n'){
+            createNode();
+        }
+    }
 }
 
 void createNode() {
+    char c;
+    while (scanf("%c", &c) != EOF && (c!='A')&&(c!='B')&&(c!='D')&&(c!='S')&&(c!='T')&&(c!='n')) {
 
+    }
 }
 
 void delete_node() {
